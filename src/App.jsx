75cc5a5e9491e9b5a893c999070e649c3c7862f5
ronitsharma03 <ronitkhajuria03>
2024-03-26 21:25:11 +0530

    Added speedInsights for testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Background from './components/Background'
 import Foreground from './components/Foreground'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
     <div className='relative w-full h-screen bg-zinc-800'>
+      <SpeedInsights />
       <Background />
       <Foreground />     
     </div>
